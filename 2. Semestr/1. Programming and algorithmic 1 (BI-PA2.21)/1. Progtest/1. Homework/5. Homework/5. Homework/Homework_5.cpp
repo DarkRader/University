eea9bb02642fd50c6e511fade26bc2803f74eb51
Artem Kuznetsov <kuznear1@fit.cvut.cz>
2022-04-09 17:33:30 +0200

    Semestr2_PA2_Homework5: do implementation method sell

--- 2. Semestr/1. Programming and algorithmic 1 (BI-PA2.21)/1. Progtest/1. Homework/5. Homework/5. Homework/Homework_5.cpp
+++ 2. Semestr/1. Programming and algorithmic 1 (BI-PA2.21)/1. Progtest/1. Homework/5. Homework/5. Homework/Homework_5.cpp
@@ -57,14 +57,30 @@ public:
         sort(vecProd.begin(), vecProd.end(), compareDate);
     }
     
-    void getIter (void)
+    int sellingProd (int sellCnt)
     {
-        sort(vecProd.begin(), vecProd.end(), compareDate);
+        for(size_t i = vecProd.size(); i > 0; i--)
+        {
+            if(vecProd[i - 1].m_count > sellCnt) {
+                vecProd[i - 1].m_count -= sellCnt;
+                m_amount -= sellCnt;
+                sellCnt = 0;
+                break;
+            } else {
+                sellCnt -= vecProd[i - 1].m_count;
+                m_amount -= vecProd[i - 1].m_count;
+                vecProd[i].m_count = 0;
+                if(sellCnt == 0)
+                    break;
+            }
+        }
+        
+        
+        return sellCnt;
     }
     
     string getName(void) const { return m_name; }
     int getAmount(void) const { return m_amount; }
-    //CDate getData(void) { return ; }
     
     struct TPRODUCT
     {
@@ -121,7 +137,59 @@ class CSupermarket
         return *this;
     }
 //-------------------------------------------------------------------------------------------------------------
-    // sell    ( shoppingList )
+    list<pair<string,int>> sell ( list<pair<string,int>> & shoppingList )
+    {
+        auto itr = shoppingList.begin();
+        
+        int notSelling = 0;
+        
+        while(itr != shoppingList.end())
+        {
+            auto itProd = products.find(itr->first);
+            
+            if(itProd != products.end())
+            {
+                notSelling = itProd->second.sellingProd(itr->second);
+                
+                if(notSelling == 0) {
+                    shoppingList.erase(itr);
+                } else {
+                    itr->second = notSelling;
+                }
+                
+                if(itProd->second.getAmount() == 0)
+                    products.erase(itProd->first);
+            } else{
+                auto itProd = products.begin();
+                
+                while(itProd != products.end())
+                {
+                    if(findMistake(itr->first, itProd->first) == false) {
+                        itProd++;
+                        continue;
+                    } else {
+                        notSelling = itProd->second.sellingProd(itr->second);
+                        
+                        if(notSelling == 0) {
+                            shoppingList.erase(itr);
+                        } else {
+                            itr->second = notSelling;
+                        }
+                        
+                        if(itProd->second.getAmount() == 0)
+                            products.erase(itProd->first);
+                    }
+                    
+                    itProd++;
+                }
+            }
+            
+            itr++;
+        }
+        
+        
+        return shoppingList;
+    }
 //-------------------------------------------------------------------------------------------------------------
     list<pair<string,int> > expired (const CDate & date) const
     {
@@ -138,24 +206,31 @@ class CSupermarket
                 int amount = 0;
                 //auto it = upper_bound(itr->second.vecProd.begin(), itr->second.vecProd.end(), itr->first);
                 auto it = itr->second.vecProd.begin();
+                
                 while(it != itr->second.vecProd.end())
                 {
-                    int amount = 0;
-                    if(date.getYear() < it->m_date.getYear())
+                    //int amount = 0;
+                    if(date.getYear() > it->m_date.getYear())
                         amount += it->m_count;
                     else if(date.getYear() == it->m_date.getYear() && date.getMonth() > it->m_date.getMonth())
                         amount += it->m_count;
                     else if (date.getYear() == it->m_date.getYear() && date.getMonth() == it->m_date.getMonth() &&
                              date.getDay() > it->m_date.getDay())
                         amount += it->m_count;
-                    else
-                        break;
+                    else {
+                        it++;
+                        continue;
+                    }
+                    
+                    //retList.push_back(make_pair(itr->first, /*itr->second.getAmount() -*/ amount));
                     
                     it++;
                 }
-                retList.push_back(make_pair(itr->first, itr->second.getAmount() - amount));
                 
-            }          
+                if(amount != 0)
+                    retList.push_back(make_pair(itr->first, /*itr->second.getAmount() -*/ amount));
+                
+            }
 
             itr++;
         }
@@ -171,6 +246,26 @@ class CSupermarket
     static bool compareCnt(const pair<string,int> &a, const pair<string,int> &b)
     {
         return (a.second > b.second);
+    }
+    
+    static bool findMistake (const string & sellProd, const string & ourProd)
+    {
+        if(sellProd.size() != ourProd.size())
+            return false;
+        
+        int check = 0;
+        
+        for(int i = 0; sellProd[i] != '\0'; i++)
+        {
+            if(sellProd[i] != ourProd[i])
+                check++;
+            
+            if(check > 1)
+                return false;
+        }
+        
+        
+        return true;
     }
     
     static bool compareDat(const CProduct::TPRODUCT & a, const CProduct::TPRODUCT & b)
@@ -196,82 +291,82 @@ int main ( void )
   list<pair<string,int> > l0 = s . expired ( CDate ( 2018, 4, 30 ) );
   assert ( l0 . size () == 4 );
   assert ( ( l0 == list<pair<string,int> > { { "bread", 200 }, { "beer", 50 }, { "butter", 10 }, { "okey", 5 } } ) );
-//
-//  list<pair<string,int> > l1 { { "bread", 2 }, { "Coke", 5 }, { "butter", 20 } };
-//  s . sell ( l1 );
-//  assert ( l1 . size () == 2 );
-//  assert ( ( l1 == list<pair<string,int> > { { "Coke", 5 }, { "butter", 10 } } ) );
-//
-//  list<pair<string,int> > l2 = s . expired ( CDate ( 2016, 4, 30 ) );
-//  assert ( l2 . size () == 1 );
-//  assert ( ( l2 == list<pair<string,int> > { { "bread", 98 } } ) );
-//
-//  list<pair<string,int> > l3 = s . expired ( CDate ( 2016, 5, 20 ) );
-//  assert ( l3 . size () == 1 );
-//  assert ( ( l3 == list<pair<string,int> > { { "bread", 198 } } ) );
-//
-//  list<pair<string,int> > l4 { { "bread", 105 } };
-//  s . sell ( l4 );
-//  assert ( l4 . size () == 0 );
-//  assert ( ( l4 == list<pair<string,int> > {  } ) );
-//
-//  list<pair<string,int> > l5 = s . expired ( CDate ( 2017, 1, 1 ) );
-//  assert ( l5 . size () == 3 );
-//  assert ( ( l5 == list<pair<string,int> > { { "bread", 93 }, { "beer", 50 }, { "okey", 5 } } ) );
-//
-//  s . store ( "Coke", CDate ( 2016, 12, 31 ), 10 );
-//
-//  list<pair<string,int> > l6 { { "Cake", 1 }, { "Coke", 1 }, { "cake", 1 }, { "coke", 1 }, { "cuke", 1 }, { "Cokes", 1 } };
-//  s . sell ( l6 );
-//  assert ( l6 . size () == 3 );
-//  assert ( ( l6 == list<pair<string,int> > { { "cake", 1 }, { "cuke", 1 }, { "Cokes", 1 } } ) );
-//
-//  list<pair<string,int> > l7 = s . expired ( CDate ( 2017, 1, 1 ) );
-//  assert ( l7 . size () == 4 );
-//  assert ( ( l7 == list<pair<string,int> > { { "bread", 93 }, { "beer", 50 }, { "Coke", 7 }, { "okey", 5 } } ) );
-//
-//  s . store ( "cake", CDate ( 2016, 11, 1 ), 5 );
-//
-//  list<pair<string,int> > l8 { { "Cake", 1 }, { "Coke", 1 }, { "cake", 1 }, { "coke", 1 }, { "cuke", 1 } };
-//  s . sell ( l8 );
-//  assert ( l8 . size () == 2 );
-//  assert ( ( l8 == list<pair<string,int> > { { "Cake", 1 }, { "coke", 1 } } ) );
-//
-//  list<pair<string,int> > l9 = s . expired ( CDate ( 2017, 1, 1 ) );
-//  assert ( l9 . size () == 5 );
-//  assert ( ( l9 == list<pair<string,int> > { { "bread", 93 }, { "beer", 50 }, { "Coke", 6 }, { "okey", 5 }, { "cake", 3 } } ) );
-//
-//  list<pair<string,int> > l10 { { "cake", 15 }, { "Cake", 2 } };
-//  s . sell ( l10 );
-//  assert ( l10 . size () == 2 );
-//  assert ( ( l10 == list<pair<string,int> > { { "cake", 12 }, { "Cake", 2 } } ) );
-//
-//  list<pair<string,int> > l11 = s . expired ( CDate ( 2017, 1, 1 ) );
-//  assert ( l11 . size () == 4 );
-//  assert ( ( l11 == list<pair<string,int> > { { "bread", 93 }, { "beer", 50 }, { "Coke", 6 }, { "okey", 5 } } ) );
-//
-//  list<pair<string,int> > l12 { { "Cake", 4 } };
-//  s . sell ( l12 );
-//  assert ( l12 . size () == 0 );
-//  assert ( ( l12 == list<pair<string,int> > {  } ) );
-//
-//  list<pair<string,int> > l13 = s . expired ( CDate ( 2017, 1, 1 ) );
-//  assert ( l13 . size () == 4 );
-//  assert ( ( l13 == list<pair<string,int> > { { "bread", 93 }, { "beer", 50 }, { "okey", 5 }, { "Coke", 2 } } ) );
-//
-//  list<pair<string,int> > l14 { { "Beer", 20 }, { "Coke", 1 }, { "bear", 25 }, { "beer", 10 } };
-//  s . sell ( l14 );
-//  assert ( l14 . size () == 1 );
-//  assert ( ( l14 == list<pair<string,int> > { { "beer", 5 } } ) );
-//
-//  s . store ( "ccccb", CDate ( 2019, 3, 11 ), 100 )
-//    . store ( "ccccd", CDate ( 2019, 6, 9 ), 100 )
-//    . store ( "dcccc", CDate ( 2019, 2, 14 ), 100 );
-//
-//  list<pair<string,int> > l15 { { "ccccc", 10 } };
-//  s . sell ( l15 );
-//  assert ( l15 . size () == 1 );
-//  assert ( ( l15 == list<pair<string,int> > { { "ccccc", 10 } } ) );
+
+  list<pair<string,int> > l1 { { "bread", 2 }, { "Coke", 5 }, { "butter", 20 } };
+  s . sell ( l1 );
+  assert ( l1 . size () == 2 );
+  assert ( ( l1 == list<pair<string,int> > { { "Coke", 5 }, { "butter", 10 } } ) );
+
+  list<pair<string,int> > l2 = s . expired ( CDate ( 2016, 4, 30 ) );
+  assert ( l2 . size () == 1 );
+  assert ( ( l2 == list<pair<string,int> > { { "bread", 98 } } ) );
+
+  list<pair<string,int> > l3 = s . expired ( CDate ( 2016, 5, 20 ) );
+  assert ( l3 . size () == 1 );
+  assert ( ( l3 == list<pair<string,int> > { { "bread", 198 } } ) );
+
+  list<pair<string,int> > l4 { { "bread", 105 } };
+  s . sell ( l4 );
+  assert ( l4 . size () == 0 );
+  assert ( ( l4 == list<pair<string,int> > {  } ) );
+
+  list<pair<string,int> > l5 = s . expired ( CDate ( 2017, 1, 1 ) );
+  assert ( l5 . size () == 3 );
+  assert ( ( l5 == list<pair<string,int> > { { "bread", 93 }, { "beer", 50 }, { "okey", 5 } } ) );
+
+  s . store ( "Coke", CDate ( 2016, 12, 31 ), 10 );
+
+  list<pair<string,int> > l6 { { "Cake", 1 }, { "Coke", 1 }, { "cake", 1 }, { "coke", 1 }, { "cuke", 1 }, { "Cokes", 1 } };
+  s . sell ( l6 );
+  assert ( l6 . size () == 3 );
+  assert ( ( l6 == list<pair<string,int> > { { "cake", 1 }, { "cuke", 1 }, { "Cokes", 1 } } ) );
+
+  list<pair<string,int> > l7 = s . expired ( CDate ( 2017, 1, 1 ) );
+  assert ( l7 . size () == 4 );
+  assert ( ( l7 == list<pair<string,int> > { { "bread", 93 }, { "beer", 50 }, { "Coke", 7 }, { "okey", 5 } } ) );
+
+  s . store ( "cake", CDate ( 2016, 11, 1 ), 5 );
+
+  list<pair<string,int> > l8 { { "Cake", 1 }, { "Coke", 1 }, { "cake", 1 }, { "coke", 1 }, { "cuke", 1 } };
+  s . sell ( l8 );
+  assert ( l8 . size () == 2 );
+  assert ( ( l8 == list<pair<string,int> > { { "Cake", 1 }, { "coke", 1 } } ) );
+
+  list<pair<string,int> > l9 = s . expired ( CDate ( 2017, 1, 1 ) );
+  assert ( l9 . size () == 5 );
+  assert ( ( l9 == list<pair<string,int> > { { "bread", 93 }, { "beer", 50 }, { "Coke", 6 }, { "okey", 5 }, { "cake", 3 } } ) );
+
+  list<pair<string,int> > l10 { { "cake", 15 }, { "Cake", 2 } };
+  s . sell ( l10 );
+  assert ( l10 . size () == 2 );
+  assert ( ( l10 == list<pair<string,int> > { { "cake", 12 }, { "Cake", 2 } } ) );
+
+  list<pair<string,int> > l11 = s . expired ( CDate ( 2017, 1, 1 ) );
+  assert ( l11 . size () == 4 );
+  assert ( ( l11 == list<pair<string,int> > { { "bread", 93 }, { "beer", 50 }, { "Coke", 6 }, { "okey", 5 } } ) );
+
+  list<pair<string,int> > l12 { { "Cake", 4 } };
+  s . sell ( l12 );
+  assert ( l12 . size () == 0 );
+  assert ( ( l12 == list<pair<string,int> > {  } ) );
+
+  list<pair<string,int> > l13 = s . expired ( CDate ( 2017, 1, 1 ) );
+  assert ( l13 . size () == 4 );
+  assert ( ( l13 == list<pair<string,int> > { { "bread", 93 }, { "beer", 50 }, { "okey", 5 }, { "Coke", 2 } } ) );
+
+  list<pair<string,int> > l14 { { "Beer", 20 }, { "Coke", 1 }, { "bear", 25 }, { "beer", 10 } };
+  s . sell ( l14 );
+  assert ( l14 . size () == 1 );
+  assert ( ( l14 == list<pair<string,int> > { { "beer", 5 } } ) );
+
+  s . store ( "ccccb", CDate ( 2019, 3, 11 ), 100 )
+    . store ( "ccccd", CDate ( 2019, 6, 9 ), 100 )
+    . store ( "dcccc", CDate ( 2019, 2, 14 ), 100 );
+
+  list<pair<string,int> > l15 { { "ccccc", 10 } };
+  s . sell ( l15 );
+  assert ( l15 . size () == 1 );
+  assert ( ( l15 == list<pair<string,int> > { { "ccccc", 10 } } ) );
 
   return EXIT_SUCCESS;
 }
