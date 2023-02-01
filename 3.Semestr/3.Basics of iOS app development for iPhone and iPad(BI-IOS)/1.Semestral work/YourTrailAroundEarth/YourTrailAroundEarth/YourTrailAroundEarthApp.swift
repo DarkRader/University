45b373a71e2752fb2ed807087b8bc1f1673005fd
Artem Kuznetsov <artyom.20century@gmail.com>
2023-02-01 16:18:38 +0100

    ✅[IOS] Add "Create project" (SW)

---
+++ 3.Semestr/3.Basics of iOS app development for iPhone and iPad(BI-IOS)/1.Semestral work/YourTrailAroundEarth/YourTrailAroundEarth/YourTrailAroundEarthApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourTrailAroundEarthApp.swift
+//  YourTrailAroundEarth
+//
+//  Created by DarkRader on 01.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct YourTrailAroundEarthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
