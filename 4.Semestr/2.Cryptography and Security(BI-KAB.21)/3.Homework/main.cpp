04f7fd0d198574a94c28f8a316dc3719fec56314
Artem Kuznetsov <artyom.20century@gmail.com>
2023-04-24 21:36:34 +0200

    🛠️[KAB] Add "Fixed open function" (HW3)
    Correct reading of data from input file

--- 4.Semestr/2.Cryptography and Security(BI-KAB.21)/3.Homework/main.cpp
+++ 4.Semestr/2.Cryptography and Security(BI-KAB.21)/3.Homework/main.cpp
@@ -42,8 +42,7 @@ bool seal(const char * inFile, const char * outFile, const char * publicKeyFile,
 
     //Generate the symetric key and IV
     unsigned char iv[EVP_MAX_IV_LENGTH];
-    int encKeyLen;
-
+    int encKeyLen = 0;
 
     //Open the input and output files
     ifstream in(inFile, ios::binary);
@@ -70,12 +69,30 @@ bool seal(const char * inFile, const char * outFile, const char * publicKeyFile,
 
     unsigned char * my_ek = (unsigned char *) malloc(EVP_PKEY_size(publicKey));
     const EVP_CIPHER *cipher = EVP_get_cipherbyname(symmetricCipher);
+    if (!cipher) {
+        cout << "Invalid cipher name" << endl;
+        out.close();
+        in.close();
+        EVP_PKEY_free(publicKey);
+        EVP_CIPHER_CTX_free(ctx);
+        return false;
+    }
 
     //Initialize the encryption context
     if(EVP_SealInit(ctx, cipher, &my_ek, &encKeyLen, iv, &publicKey, 1) != 1) {
         cout << "Failed to initialize OpenSSL cipher context" << endl;
         EVP_CIPHER_CTX_free(ctx);
         EVP_PKEY_free(publicKey);
+        return false;
+    }
+
+    int nid = EVP_CIPHER_nid(cipher);
+    out.write(reinterpret_cast<char*>(&nid), sizeof(nid));
+    if(!out.good()) {
+        cout << "Can't writting nid header to file " << outFile << endl;
+        out.close();
+        EVP_PKEY_free(publicKey);
+        EVP_CIPHER_CTX_free(ctx);
         return false;
     }
 
@@ -138,57 +155,93 @@ bool seal(const char * inFile, const char * outFile, const char * publicKeyFile,
 
 bool open(const char *inFile, const char *outFile, const char *privateKeyFile) {
     // Load the private key from file
-    FILE *fp = fopen(privateKeyFile, "r");
+    FILE *fp = fopen(privateKeyFile, "rb");
     if (!fp) {
-        cerr << "Error: could not open private key file: " << privateKeyFile << endl;
+        cout << "Could not open private key file: " << privateKeyFile << endl;
         return false;
     }
 
     EVP_PKEY *privateKey = PEM_read_PrivateKey(fp, NULL, NULL, NULL);
     fclose(fp);
     if (!privateKey) {
-        cerr << "Error: could not load private key from file: " << privateKeyFile << endl;
+        cout << "Could not load private key from file: " << privateKeyFile << endl;
         return false;
     }
 
     // Open the input and output files
     ifstream in(inFile, ios::binary);
     if (!in.is_open()) {
-        cerr << "Error: could not open input file: " << inFile << endl;
+        cout << "Could not open input file: " << inFile << endl;
         EVP_PKEY_free(privateKey);
         return false;
     }
 
     ofstream out(outFile, ios::binary);
     if (!out.is_open()) {
-        cerr << "Error: could not open output file: " << outFile << endl;
+        cout << "Could not open output file: " << outFile << endl;
+        EVP_PKEY_free(privateKey);
+        return false;
+    }
+
+    int nid = 0;
+    in.read(reinterpret_cast<char *>(&nid), sizeof(nid));
+    if(!in.good()) {
+        cout << "Failed to read nid from input file" << endl;
         EVP_PKEY_free(privateKey);
         return false;
     }
 
     //Read the IV from the input file
-    unsigned  char iv[EVP_MAX_IV_LENGTH];
-    in.read(reinterpret_cast<char*>(iv), EVP_MAX_IV_LENGTH);
+//    unsigned  char iv[EVP_MAX_IV_LENGTH];
+    int encKeyLen = 0;
+    in.read(reinterpret_cast<char*>(&encKeyLen), sizeof(encKeyLen));
     if(!in.good()) {
-        cout << "Failed to read IV from input file" << endl;
+        cout << "Failed to read encrypted key length from input file" << endl;
         EVP_PKEY_free(privateKey);
         return false;
+    }
+
+    //Read the encrypted key from the input file
+    unsigned char * my_ek = (unsigned char *) malloc(EVP_PKEY_size(privateKey));
+    in.read(reinterpret_cast<char*>(my_ek), encKeyLen);
+    if(!in.good()) {
+        cout << "Failed to read encrypted key from input file" << endl;
+        EVP_PKEY_free(privateKey);
+        return false;
+    }
+
+    //Read the IV from the input file
+    unsigned char iv[EVP_MAX_IV_LENGTH];
+    int ivLen = EVP_CIPHER_iv_length(EVP_get_cipherbynid(nid));
+    if (ivLen > 0) {
+        in.read(reinterpret_cast<char*>(iv), ivLen);
+        if(!in.good()) {
+            cout << "Failed to read IV from input file" << endl;
+            EVP_PKEY_free(privateKey);
+            return false;
+        }
     }
 
     //Prepare the context
     EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
     if (!ctx) {
-        cerr << "Error: could not create cipher context" << endl;
+        cout << "Could not create cipher context" << endl;
         EVP_PKEY_free(privateKey);
+        EVP_CIPHER_CTX_free(ctx);
         return false;
     }
 
-    const char* K_SYMMETRIC_CIPHER = "AES-256-CBC";
-    const EVP_CIPHER  *cipher = EVP_get_cipherbyname(K_SYMMETRIC_CIPHER);
+    const EVP_CIPHER *cipher = EVP_get_cipherbynid(nid);
+    if(!cipher) {
+        cout << "Invalid cipher" << endl;
+        EVP_PKEY_free(privateKey);
+        EVP_CIPHER_CTX_free(ctx);
+        return false;
+    }
 
     //Initialize the decryption context
-    if (!EVP_OpenInit(ctx, cipher, NULL, 0, NULL, privateKey)) {
-        cerr << "Error: could not initialize decryption operation" << endl;
+    if (EVP_OpenInit(ctx, cipher, my_ek, encKeyLen, iv, privateKey) != 1) {
+        cout << "Could not initialize decryption operation" << endl;
         EVP_CIPHER_CTX_free(ctx);
         EVP_PKEY_free(privateKey);
         return false;
@@ -201,7 +254,7 @@ bool open(const char *inFile, const char *outFile, const char *privateKeyFile) {
 
     //Read data from the input file, decrypt it using the EVP_OpenUpdate function, and write it to the output file
     while (in.good()) {
-        in.read(reinterpret_cast<char*>(inBuffer), K_BUFFER_SIZE + EVP_MAX_BLOCK_LENGTH);
+        in.read(reinterpret_cast<char*>(inBuffer), K_BUFFER_SIZE);
         int inLenght = (int)in.gcount();
 
         if (EVP_OpenUpdate(ctx, outBuffer, &outLenght, inBuffer, inLenght) != 1) {
@@ -224,7 +277,7 @@ bool open(const char *inFile, const char *outFile, const char *privateKeyFile) {
 
     // Finalize the decryption operation
     if (EVP_OpenFinal(ctx, outBuffer, &outLenght) != 1) {
-        cerr << "Error: decryption error" << endl;
+        cout << "Decryption error" << endl;
         EVP_CIPHER_CTX_free(ctx);
         EVP_PKEY_free(privateKey);
         return false;
@@ -236,6 +289,7 @@ bool open(const char *inFile, const char *outFile, const char *privateKeyFile) {
         cout << "Failed to write decrypted data to output file" << endl;
         out.close();
         EVP_CIPHER_CTX_free(ctx);
+        EVP_PKEY_free(privateKey);
         return false;
     }
 
@@ -249,10 +303,11 @@ bool open(const char *inFile, const char *outFile, const char *privateKeyFile) {
 #ifndef __PROGTEST__
 
 int main(void) {
-    assert( seal("test/fileToEncrypt", "sealed.bin", "tests/PublicKey.pem", "aes-128-cbc") );
-    assert( open("sealed.bin", "openedFileToEncrypt", "tests/PrivateKey.pem") );
+//    assert( seal("test/fileToEncrypt", "sealed.bin", "tests/PublicKey.pem", "aes-128-cbc") );
+//    assert( open("sealed.bin", "openedFileToEncrypt", "tests/PrivateKey.pem") );
 
     assert(open("tests/sealed_sample.bin", "opened_sample.txt", "tests/PrivateKey.pem"));
+    cout << "Finish" << endl;
 
     return 0;
 }
