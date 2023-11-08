fb45c0991fb7a1fa4a5b14108aaa78da27dcd700
Artem Kuznetsov <artyom.20century@gmail.com>
2023-11-08 14:32:00 +0100

    ✴️[AG1] Add "Final version of the program" (HW2)

--- 5.Semestr/1.Algorithms and Graphs 1(BI-AG1)/1.Progtest/2.Homework/Homework_2.cpp
+++ 5.Semestr/1.Algorithms and Graphs 1(BI-AG1)/1.Progtest/2.Homework/Homework_2.cpp
@@ -1,23 +1,8 @@
 #ifndef __PROGTEST__
-#include <cassert>
 #include <iomanip>
-#include <cstdint>
 #include <iostream>
 #include <memory>
-#include <limits>
-#include <optional>
-#include <algorithm>
 #include <bitset>
-#include <list>
-#include <array>
-#include <vector>
-#include <deque>
-#include <unordered_set>
-#include <unordered_map>
-#include <stack>
-#include <queue>
-#include <random>
-#include <type_traits>
 
 #endif
 
@@ -27,37 +12,69 @@ class CTree {
 protected:
     class CNode;
 public:
-    CTree(void) = default;
-    ~CTree(void) {
+    CTree() = default;
+    ~CTree() {
         delete m_root;
-//        for (CNode* node : m_lines) {
-//            delete node;
-//        }
     }
 
-    CNode * getRoot(void) const {
+    CNode * getRoot() const {
         return m_root;
     }
 
-//    vector<CNode*> getLines(void) const {
-//        return m_lines;
-//    }
-
-    size_t getSizeSubTree(CNode * node) const {
+    // Get the size of the SubTree (number of elements)
+    static size_t getSizeSubTree(CNode * node) {
         if(node != nullptr) {
             return node->m_subTreeSize;
         }
         return 0;
     }
 
-    int getDepthSize(CNode * node) const {
+    static size_t getLeftIndex(CNode * node) {
+        if(!node) {
+            return 0;
+        } else {
+            return node->m_subTreeSize;
+        }
+    }
+
+    static size_t getLeftLines(CNode * node) {
+        if(!node) {
+            return 0;
+        } else {
+            return node->m_newLines;
+        }
+    }
+
+    static size_t getNewLinesTree(CNode * node) {
+        size_t newLines = 0;
+        if (node->m_L) {
+            newLines += node->m_L->m_newLines;
+        }
+        if (node->m_key == '\n') {
+            newLines++;
+        }
+        if (node->m_R) {
+            newLines += node->m_R->m_newLines;
+        }
+        return newLines;
+    }
+
+    static void incLines(CNode * node, char sym) {
+        if(sym == '\n') {
+            node->m_newLines++;
+        }
+    }
+
+    // Get the depth of a subtree rooted at a given node
+    static int getDepthSize(CNode * node) {
         if(node != nullptr) {
             return node->m_depth;
         }
         return 0;
     }
 
-    int maxDepth(CNode * leftNode, CNode * rightNode) {
+    // Get the maximum depth between two subtrees
+    static int maxDepth(CNode * leftNode, CNode * rightNode) {
         if(getDepthSize(leftNode) < getDepthSize(rightNode)) {
             return getDepthSize(rightNode);
         } else {
@@ -65,258 +82,385 @@ public:
         }
     }
 
+    // Find the minimum value node in a given subtree
+    CNode * minValue(CNode * node) {
+        if(node->m_L == nullptr) {
+            return node;
+        } else {
+            return minValue(node->m_L);
+        }
+    }
+
+    static size_t charToLine(CNode* node, size_t i, size_t newLines) {
+        if(!node) {
+            return 0;
+        }
+
+        size_t leftIndex = getLeftIndex(node->m_L);
+
+        if(i < leftIndex) {
+            return charToLine(node->m_L, i, newLines);
+        } else if(i > leftIndex) {
+            if(node->m_key == '\n') {
+                if(node->m_L) {
+                    newLines += node->m_L->m_newLines + 1;
+                } else {
+                    newLines++;
+                }
+            } else {
+                if(node->m_L) {
+                    newLines += node->m_L->m_newLines;
+                }
+            }
+            return charToLine(node->m_R, i - leftIndex - 1, newLines);
+        } else {
+            if(node->m_L) {
+                newLines += node->m_L->m_newLines;
+            }
+            return newLines;
+        }
+    }
+
+    size_t lineStart(CNode* node, size_t r, size_t position) const {
+        if (!node) {
+            return position;
+        }
+
+        size_t leftIndex = getLeftIndex(node->m_L);
+        size_t leftLines = getLeftLines(node->m_L);
+
+        if(r < leftLines) {
+            return lineStart(node->m_L, r, position);
+        } else if(r > leftLines) {
+            if(node->m_key == '\n') {
+                return lineStart(node->m_R, r - leftLines - 1, leftIndex + 1 + position);
+            }
+            return lineStart(node->m_R, r - leftLines, leftIndex + 1 + position);
+        } else if(node->m_key == '\n') {
+            position += leftIndex + 1;
+            return position;
+        } else {
+            return lineStart(node->m_R, r - leftLines, leftIndex + 1 + position);
+        }
+    }
+
+    // Recursive helper function to find an element by index
     char findSymbol(CNode * node, size_t ind) const {
-        char symbol;
-        if((node->m_L == nullptr && ind == 0) || (node->m_L && ind == node->m_L->m_subTreeSize)) {
-            return node->m_key;
-        } else if(node->m_L && ind < node->m_L->m_subTreeSize) {
-            symbol = findSymbol(node->m_L, ind);
-        } else if(node->m_R) {
-            if(node->m_L) {
-                symbol = findSymbol(node->m_R, ind - node->m_L->m_subTreeSize - 1);
+        if(node == nullptr || ind >= node->m_subTreeSize) {
+            throw std:: out_of_range("out of range in at");
+        }
+
+        if(node->m_L != nullptr) {
+            size_t leftSize = node->m_L->m_subTreeSize;
+            if(ind == leftSize) {
+                return node->m_key;
+            } else if(ind < leftSize) {
+                return findSymbol(node->m_L, ind);
             } else {
-                symbol = findSymbol(node->m_R, ind - 1);
+                return findSymbol(node->m_R, ind - leftSize - 1);
+            }
+        } else {
+            if(ind == 0) {
+                return node->m_key;
+            } else {
+                return findSymbol(node->m_R, ind - 1);
             }
         }
-        return symbol;
     }
 
-    char edit(CNode * node, size_t ind, char toChange) {
-        char symbol;
-        if((node->m_L == nullptr && ind == 0) || (node->m_L && ind == node->m_L->m_subTreeSize)) {
-            symbol = node->m_key;
+    // Found the node to edit
+    static CNode * nodeToEdit(CNode * node, char toChange) {
+        if (node->m_key == '\n' && toChange != '\n') {
             node->m_key = toChange;
-            return symbol;
-        } else if(node->m_L && ind < node->m_L->m_subTreeSize) {
-            symbol = edit(node->m_L, ind, toChange);
-        } else if(node->m_R) {
-            if(node->m_L) {
-                symbol = edit(node->m_R, ind - node->m_L->m_subTreeSize - 1, toChange);
-            } else {
-                symbol = edit(node->m_R, ind - 1, toChange);
-            }
+            node->m_newLines = 0;
+        } else if (node->m_key != '\n' && toChange == '\n') {
+            node->m_key = toChange;
+            node->m_newLines = 1;
+        } else {
+            node->m_key = toChange;
         }
-        return symbol;
+        return node;
     }
 
-    CNode * prepInsert(char key, size_t ind, bool isInsert) {
-        if(isInsert == true) {
+    char prepEdit(size_t ind, char toChange) {
+        CNode *nodeChange = edit(m_root, ind, toChange);
+        char oldChar = nodeChange->m_key;
+        nodeChange->m_key = toChange;
+        return oldChar;
+    }
+
+    CNode * edit(CNode * node, size_t ind, char toChange) {
+        if(node == nullptr || ind >= node->m_subTreeSize) {
+            throw std:: out_of_range("out of range in edit");
+        }
+
+        CNode *findNode;
+
+        if (node->m_L != nullptr) {
+            size_t leftSize = node->m_L->m_subTreeSize;
+            if (ind == leftSize) {
+                findNode = nodeToEdit(node, toChange);
+            } else if (ind < leftSize) {
+                findNode = edit(node->m_L, ind, toChange);
+            } else {
+                if (node->m_L) {
+                    findNode = edit(node->m_R, ind - leftSize - 1, toChange);
+                } else {
+                    findNode = edit(node->m_R, ind - 1, toChange);
+                }
+            }
+        } else {
+            if (ind == 0) {
+                findNode = nodeToEdit(node, toChange);
+            } else {
+                findNode = edit(node->m_R, ind - 1, toChange);
+            }
+        }
+
+        node->m_newLines = getNewLinesTree(node);
+
+        return findNode;
+    }
+
+    void prepInsertOrDelete(char key, size_t ind, bool isInsert) {
+        if(isInsert) {
             m_root = insert(m_root, key, ind);
         } else {
-            m_root = del(m_root, key, ind);
+            m_root = del(m_root, ind);
         }
     }
 
-    CNode * del(CNode * node, char key, size_t ind) {
+    // Recursive helper function to remove an element from the tree
+    CNode * del(CNode * node, size_t ind) {
         if(node == nullptr) {
             return nullptr;
+        } else if((ind == 0 && !node->m_L) || (node->m_L && ind == node->m_L->m_subTreeSize)) {
+            if(node->m_L == nullptr && node->m_R == nullptr) {
+                delete node;
+                return nullptr;
+            } else if(node->m_L == nullptr) {
+                auto tmp = node->m_R;
+                node->m_R = nullptr;
+                delete node;
+                return tmp;
+            } else if(node->m_R == nullptr) {
+                auto tmp = node->m_L;
+                node->m_L = nullptr;
+                delete node;
+                return tmp;
+            }
+            CNode * tmpNode = minValue(node->m_R);
+            node->m_key = tmpNode->m_key;
+            node->m_R = del(node->m_R, 0);
+            node->m_depth = maxDepth(node->m_L, node->m_R) + 1;
+            node->m_subTreeSize--;
+            node->m_newLines = getNewLinesTree(node);
+            node = rotationTree(node);
+        } else if((node->m_L == nullptr && ind < node->m_subTreeSize - 1) || (node->m_L && ind < node->m_L->m_subTreeSize)) {
+            node->m_L = del(node->m_L, ind);
+            node->m_depth = maxDepth(node->m_L, node->m_R) + 1;
+            node->m_subTreeSize--;
+            node->m_newLines = getNewLinesTree(node);
+            node = rotationTree(node);
+        } else {
+            if(node->m_L) {
+                node->m_R = del(node->m_R, ind - node->m_L->m_subTreeSize - 1);
+            } else {
+                node->m_R = del(node->m_R, ind - 1);
+            }
+            node->m_depth = maxDepth(node->m_L, node->m_R) + 1;
+            node->m_subTreeSize--;
+            node->m_newLines = getNewLinesTree(node);
+            node = rotationTree(node);
         }
+        return node;
     }
 
+    // Recursive helper function to insert a new element into the tree
     CNode * insert(CNode * node, char key, size_t ind) {
         if(node == nullptr) {
             node = new CNode(key);
             node->m_depth = 1;
             node->m_subTreeSize++;
+            incLines(node, key);
             return node;
         }
 
-        if(ind == 0) {
+        size_t leftIndex = getLeftIndex(node->m_L);
+
+        if(ind <= leftIndex) {
             node->m_subTreeSize++;
+            incLines(node, key);
             node->m_L = insert(node->m_L, key, ind);
             node->m_depth = maxDepth(node->m_L, node->m_R) + 1;
             node = rotationTree(node);
-        } else if (node->m_L && ind == node->m_L->m_subTreeSize) {
-            node->m_subTreeSize++;
-            CNode* newNode = new CNode(key);
-            newNode->m_L = node->m_L;
-            node->m_L = newNode;
-            newNode->m_subTreeSize = newNode->m_L->m_subTreeSize + 1;
-            newNode->m_depth = maxDepth(newNode->m_L, newNode->m_R) + 1;
-//            newNode = rotationTree(newNode);
-        } else if((node->m_L == nullptr && ind < node->m_subTreeSize) || (node->m_L && ind < node->m_L->m_subTreeSize)) {
-            node->m_subTreeSize++;
-            node->m_L = insert(node->m_L, key, ind);
-            node->m_depth = maxDepth(node->m_L, node->m_R) + 1;
-            node = rotationTree(node);
-        } else if ((node->m_R == nullptr && ind > node->m_subTreeSize - 1) || (node->m_R && ind > node->m_R->m_subTreeSize - 1)) {
+        } else {
             node->m_subTreeSize++;
             if(node->m_L) {
-                node->m_R = insert(node->m_R, key, ind - node->m_L->m_subTreeSize - 1);
+                node->m_R = insert(node->m_R, key, ind - leftIndex - 1);
             } else {
                 node->m_R = insert(node->m_R, key, ind - 1);
             }
+            incLines(node, key);
             node->m_depth = maxDepth(node->m_L, node->m_R) + 1;
             node = rotationTree(node);
         }
-        
         return node;
     }
 
-    CNode * rotationTree(CNode * node) {
+    // Perform a rotation on a node to maintain balance
+    static CNode * rotationTree(CNode * node) {
+        if (node == nullptr) {
+            return nullptr;
+        }
+
         if(2 <= getDepthSize(node->m_R) - getDepthSize(node->m_L)) {
             if(0 > getDepthSize(node->m_R->m_R) - getDepthSize(node->m_R->m_L)) {
                 node->m_R = rotateRight(node->m_R);
             }
             node = rotateLeft(node);
         } else if(-2 >= getDepthSize(node->m_R) - getDepthSize(node->m_L)) {
-            if(0 < getDepthSize(node->m_L->m_R) - getDepthSize(node->m_L->m_L)) {
+            if (0 < getDepthSize(node->m_L->m_R) - getDepthSize(node->m_L->m_L)) {
                 node->m_L = rotateLeft(node->m_L);
             }
             node = rotateRight(node);
         }
+        node->m_newLines = getNewLinesTree(node);
         return node;
     }
 
-    CNode * rotateLeft(CNode * node) {
+    // Rotate a node to the left
+    static CNode * rotateLeft(CNode * node) {
         CNode * rotateNode = node->m_R;
         node->m_R = rotateNode->m_L;
         rotateNode->m_L = node;
         node->m_depth = maxDepth(node->m_L, node->m_R) + 1;
         node->m_subTreeSize = getSizeSubTree(node->m_L) + getSizeSubTree(node->m_R) + 1;
+        node->m_newLines = getNewLinesTree(node);
         rotateNode->m_depth = maxDepth(rotateNode->m_R, node) + 1;
         rotateNode->m_subTreeSize = getSizeSubTree(rotateNode->m_L) + getSizeSubTree(rotateNode->m_R) + 1;
+        rotateNode->m_newLines = getNewLinesTree(rotateNode);
         return rotateNode;
     }
 
-    CNode * rotateRight(CNode * node) {
+    // Rotate a node to the right
+    static CNode * rotateRight(CNode * node) {
         CNode * rotateNode = node->m_L;
         node->m_L = rotateNode->m_R;
         rotateNode->m_R = node;
         node->m_depth = maxDepth(node->m_L, node->m_R) + 1;
         node->m_subTreeSize = getSizeSubTree(node->m_L) + getSizeSubTree(node->m_R) + 1;
+        node->m_newLines = getNewLinesTree(node);
         rotateNode->m_depth = maxDepth(rotateNode->m_L, node) + 1;
         rotateNode->m_subTreeSize = getSizeSubTree(rotateNode->m_L) + getSizeSubTree(rotateNode->m_R) + 1;
+        rotateNode->m_newLines = getNewLinesTree(rotateNode);
         return rotateNode;
     }
 
 protected:
-
+    // Nested class for tree nodes
     class CNode {
     public:
-        CNode(void) {}
-        ~CNode(void) {
+        explicit CNode(char key) : m_key(key) {}
+        ~CNode() {
             delete m_L;
             delete m_R;
         }
 
-        CNode(char key) {
-            m_key = key;
-        }
-
+        size_t m_newLines = 0;
         int m_depth = 0;
         size_t m_subTreeSize = 0;
         char m_key;
         CNode * m_L = nullptr;
         CNode * m_R = nullptr;
     };
-//    vector<CNode*> m_lines;
     CNode * m_root = nullptr;
 };
 
 struct TextEditorBackend {
     CTree tree;
-    int m_lines = 0;
-    vector<size_t> vecLines;
 
-    TextEditorBackend(const std::string& text) {
+    explicit TextEditorBackend(const std::string& text) {
         for(size_t i = 0; i < text.size(); i++) {
             insert(i, text[i]);
         }
     }
 
     size_t size() const {
-        return tree.getSizeSubTree(tree.getRoot());
+        return CTree::getSizeSubTree(tree.getRoot());
     }
 
     size_t lines() const {
-        return vecLines.size() + 1;
+        return tree.getRoot()->m_newLines + 1;
     }
 
     char at(size_t i) const {
-        if(i < 0 || i > size()) {
+        if(i >= size()) {
             throw std:: out_of_range("out of range in at");
         }
         return tree.findSymbol(tree.getRoot(), i);
     }
 
     void edit(size_t i, char c) {
-        if(i < 0 || i > size()) {
-            throw std:: out_of_range("out of range in edit");
-        }
-        if(tree.edit(tree.getRoot(), i, c) == '\n') {
-            for(size_t t = 0; t < vecLines.size(); t++) {
-                if(vecLines[t] == i) {
-                    vecLines.erase(vecLines.begin() + t);
-                }
-            }
-        }
+        tree.prepEdit(i, c);
     }
+
     void insert(size_t i, char c) {
-        tree.prepInsert(c, i, true);
-        if(c == '\n') {
-            m_lines++;
-            vecLines.push_back(i);
+        if(i > size()) {
+            throw std:: out_of_range("out of range in insert");
         }
+
+        tree.prepInsertOrDelete(c, i, true);
     }
 
     void erase(size_t i) {
+        if(i >= size()) {
+            throw std:: out_of_range("out of range in erase");
+        }
+
+        tree.prepInsertOrDelete('\0', i, false);
     }
 
     size_t line_start(size_t r) const {
-        if(r < 0 || r > vecLines.size()) {
+        if(r >= lines()) {
             throw std:: out_of_range("out of range in line start");
         }
 
         if(r == 0) {
             return 0;
-        } else if(vecLines.size() == r) {
-            return vecLines[r - 1] + 1;
         } else {
-            return vecLines[r - 1] + 1;
+            size_t control = tree.lineStart(tree.getRoot(), r - 1, 0);
+            return control;
         }
-
-        return 0;
     }
 
     size_t line_length(size_t r) const {
-        if(r < 0 || r > vecLines.size()) {
+        if(r >= lines()) {
             throw std:: out_of_range("out of range in line length");
         }
 
-        if(r == 0) {
-            if(vecLines.size() == 0) {
-                return size();
-            } else {
-                return vecLines[r] + 1;
-            }
-        } else if(vecLines.size() == r) {
-            return size() - vecLines[r - 1] - 1;
+        if(r == 0 && lines() == 1) {
+            return size();
+        } else if(lines() == r + 1) {
+            return size() - tree.lineStart(tree.getRoot(), r - 1, 0);
         } else {
-            return vecLines[r] - vecLines[r - 1];
+            size_t leftLine;
+            if(r == 0) {
+                return leftLine = tree.lineStart(tree.getRoot(), 0, 0);
+            } else {
+                leftLine = tree.lineStart(tree.getRoot(), r - 1, 0) - 1;
+            }
+            size_t rightLine = tree.lineStart(tree.getRoot(), r, 0) - 1;
+            return rightLine - leftLine;
         }
-
-        return 0;
     }
 
     size_t char_to_line(size_t i) const {
-        if(i < 0 || i > size()) {
-            throw std:: out_of_range("out of range in char to line");
+        if(i >= size()) {
+            throw std:: out_of_range("out of range in line char to line");
         }
-
-        for(size_t t = 0; t < vecLines.size(); t++) {
-            if(vecLines[t] == i) {
-                return t;
-            }
-            if(i < vecLines[t]) {
-                return t;
-            }
-            if(vecLines.size() == t + 1 && i > vecLines[t]) {
-                return t + 1;
-            }
-        }
-
-        return 0;
+        size_t control = CTree::charToLine(tree.getRoot(), i, 0);
+        return control;
     }
 };
 
